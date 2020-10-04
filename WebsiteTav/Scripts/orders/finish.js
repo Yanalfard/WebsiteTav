9628739813b461600e6bfdc42a6ec82d7edcdba3
nazila bagheri <nazila.bagheri93@gmail.com>
2020-10-04 11:04:44 +0330

    map updated on footer and about us

---
+++ WebsiteTav/Scripts/orders/finish.js
@@ -0,0 +1,24 @@
+﻿const pricOFplan = parseInt(localStorage.getItem("priceOfPlan"));
+const userOfPlan = localStorage.getItem("userOfPlan");
+const timeOfPlan = localStorage.getItem("timeOfPlan");
+const videoOfPlan = localStorage.getItem("videoOfPlan");
+const totalPrice = parseInt(localStorage.getItem("totalPrice"));
+const periodOfplan = localStorage.getItem("periodOfplan");
+
+document.getElementsByClassName("usercount")[0].innerHTML = userOfPlan;
+document.getElementsByClassName("timeINmonth")[0].innerHTML = timeOfPlan;
+document.getElementsByClassName("videocount")[0].innerHTML = videoOfPlan;
+document.getElementsByClassName("periodInfinish")[0].innerHTML = periodOfplan;
+document.getElementsByClassName("priceInfinish")[0].innerHTML = `${pricOFplan} تومان`;
+//document.getElementsByClassName("priceInfinish")[0].innerHTML = format("{0:#,###0.#}", pricOFplan);
+//document.getElementsByClassName("priceInfinish")[0].innerHTML = pricOFplan.replace(/(\d{3})(?=\d)/g, '$1.')
+
+
+document.getElementsByClassName("totalpriceInfinish")[0].innerHTML = `${totalPrice} تومان`;
+
+document.getElementsByClassName("usercount")[1].innerHTML = `سرویس ${userOfPlan} `;
+document.getElementsByClassName("periodInfinish")[1].innerHTML = periodOfplan;
+document.getElementsByClassName("totalpriceInfinish")[1].innerHTML = `${totalPrice} تومان`;
+
+document.getElementsByClassName("capacity")[0].innerHTML = `${(userOfPlan.match(/(\d+)/)[0]) * (timeOfPlan.match(/(\d+)/)[0])} نفرساعت  ` ;
+
---
+++ WebsiteTav/Scripts/orders/navigation.js
@@ -0,0 +1,28 @@
+﻿function btnChoosePlan_Click() {
+    window.location.assign("order.html");
+}
+
+let pirce = "";
+
+//localStorage.setItem("VarName", "VarValue");
+//localStorage.getItem("VarName");
+
+function periodOfPlan_Click() {
+   // debugger;
+    const a = localStorage.getItem("priceOfPlan");
+    if (a == null || a == "null") {
+        UIkit.notification("یکی از پلن ها را انتخاب کنید")
+        return;
+    }
+
+    window.location.assign("period.html");
+}
+function finish_Click() {
+    window.location.assign("finish.html");
+}
+
+//function myfunction() {
+//    return "hello";
+//} if (myfunction == "hello") {
+//    alert('Yes');
+//}
