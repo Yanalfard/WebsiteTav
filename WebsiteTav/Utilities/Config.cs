22a908a5dfc20bdf79cbab9ee942c71901ce7b6f
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-11-02 15:21:33 +0330

    Connection String Changed

--- WebsiteTav/Utilities/Config.cs
+++ WebsiteTav/Utilities/Config.cs
@@ -2,6 +2,7 @@ namespace WebsiteTav.Utilities
 {
     public static class Config
     {
-        public static readonly string ConnectionString = "Data Source=109.169.76.79;Initial Catalog=arvintav_WebsiteTav;User ID=arvintav_Root;Password=1710ahmad.fard";
+        //public static readonly string ConnectionString = "Data Source=109.169.76.79;Initial Catalog=arvintav_WebsiteTav;User ID=arvintav_Root;Password=1710ahmad.fard";
+        public static readonly string ConnectionString = "Data Source=103.216.62.27;Initial Catalog=WebsiteTav;User ID=Yanal;Password=1710ahmad.fard";
     }
 }
